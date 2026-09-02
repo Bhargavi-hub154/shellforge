92b9fd1ad8051f57d0217fcf13ba4efb30157a7d
Bhargavi-hub154 <240132164+Bhargavi-hub154@users.noreply.github.com>
2026-09-02 10:49:52 +0000

    Milestone-3

--- src/main.c
+++ src/main.c
@@ -3,10 +3,14 @@
 #include <string.h>
 #include <readline/history.h>
 #include <readline/readline.h>
+#include "history.h"
 #include "token.h"
 #include "lexer.h"
 #include "parser.h"
 #include "expand.h"
+#include "builtin.h"
+#include "executor.h"
+
 int main(void)
 {
     // Display a welcome banner when the shell starts
@@ -14,8 +18,8 @@ int main(void)
     printf("      Shellforge \n");
     printf(" A Unix Style Shell written in C\n");
     printf("=====================================\n");
-
-  token_list_t tokens;
+ using_history();
+ token_list_t tokens;
  pipeline_t pipeline;
  
  char *line;
@@ -34,26 +38,41 @@ int main(void)
             continue;
         }
 
-         add_history(line);
+       if (strcmp(line, "history") == 0)
+       {
+          print_history();
+          free(line);
+           continue;
+       }
+// milestone 1 - enabling history
+
+        add_history(line);
+
+// milestone 2.1 - tokenization and lexer
+
 	lexer(line, &tokens);
-        token_print(&tokens);
-        
+
+        // token_print(&tokens);
+
+// milestone 2.2 - expansion of environment variables and parser
 
 	if(parser(&tokens, &pipeline))
 	{
 		expand_variables(&pipeline);
-    		pipeline_print(&pipeline);
+    	//	pipeline_print(&pipeline);
 	}
-        
 
 
-        if (strcmp(line, "exit") == 0)
-        {
-            free(line);
-            printf("Exiting...\n");
-            break;
-        }
-	free(line);
-    }    
+	if (pipeline.command_count == 1 &&  pipeline.commands[0].argc > 0 && strcmp(pipeline.commands[0].argv[0],"exit") == 0)
+         {
+                free(line);
+                break;
+            }
+
+        execute_pipeline(&pipeline);
+
+       free(line);
+
+    }
     return 0;
 }
